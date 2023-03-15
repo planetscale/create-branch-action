a45900d684dcb0657b3b6e882a659b20d5651c00
Brian Morrison II <brian@brianmorrison.me>
2023-03-15 15:35:11 -0400

    use new helpers

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 # Container image that runs your code
 FROM planetscale/pscale:v0.129.0
 
-RUN apk --no-cache add bash jq
+RUN apk --no-cache add bash jq curl
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
-COPY install-helpers.sh /install-helpers.sh
+RUN chmod +x entrypoint.sh
 
 # Install the PlanetScale Actions helpers
+COPY install-helpers.sh /install-helpers.sh
+RUN chmod +x /install-helpers.sh
 RUN /install-helpers.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
--- install-helpers.sh
+++ install-helpers.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-PSCALE_CLI_HELPER_SCRIPTS_NAME=pscale-workflow-helper-scripts
-RELEASE_VERSION=781fa51d4cbb488cba6613e3d4250198e27cc7a7
-PSCALE_SCRIPTS_DIR=.pscale/
-
-wget -O ${PSCALE_CLI_HELPER_SCRIPTS_NAME}.zip https://github.com/planetscale/pscale-workflow-helper-scripts/archive/${RELEASE_VERSION}.zip
-unzip -o ${PSCALE_CLI_HELPER_SCRIPTS_NAME}.zip
-
-# create .pscale directory
-mkdir -p ${PSCALE_SCRIPTS_DIR}
-
-# copy scripts to .pscale directory
-cp -r ${PSCALE_CLI_HELPER_SCRIPTS_NAME}-${RELEASE_VERSION}/.pscale/cli-helper-scripts ${PSCALE_SCRIPTS_DIR}/
-
-# remove zip file and extracted directory
-rm ${PSCALE_CLI_HELPER_SCRIPTS_NAME}.zip
-rm -rf ${PSCALE_CLI_HELPER_SCRIPTS_NAME}-${RELEASE_VERSION}
-
-echo
-echo "Successfully installed pscale-workflow-helper-scripts"
-echo
-
-
-
-
-
-
+curl -L -o install.sh https://raw.githubusercontent.com/planetscale/pscale-workflow-helper-scripts/main/install.sh
+chmod +x /install.sh
+. /install.sh -s -t 0.7
