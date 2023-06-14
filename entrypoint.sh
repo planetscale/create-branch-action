cd2d9ec6981b115f24db5946f677ea3301e650de
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2023-06-14 14:50:03 -0400

    remove extra echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,6 @@ if [ -n "$6" ];then
   command="$command --region $6"
 fi
 
-echo $8
-
 # Check if branch already exists
 if [ "true" == "$8" ];then
   output=$(eval "pscale branch show $1 $2 --org $3" 2>&1)
